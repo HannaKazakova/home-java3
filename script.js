dff0b100e10cda35cbb5bb48591b417bd59684eb
Hanna <kazkovaanna76@mail.ru>
2022-05-29 13:57:47 +0300

    е
    save decisions

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ for(let j = 35; j <= 35; j-- ) {
     console.log(j);
 }
 
+
+
+//3
+
+let b = 100;
+let c = 0;
+for(let i = 0; i <= b; i++){
+    c += i;
+}
+console.log(c);
+
+
 //4
 
 let a = 5;
@@ -22,11 +34,41 @@ for(let i = 1; i <= a; i++ ){
     console.log('Sum' + i + '=' + sum);
 }  
 
-//3
+//2
 
-let b = 100;
-//let sum = 0;
-for(let i = 0; i <= b; i++){
-    sum += i;
+i = 89;
+while( i >= 11) {
+    document.write(i + '<br/>');
+    i--;
 }
-console.log(sum);
+
+//5
+
+let f = 0;
+for(let f = 8; f <= 56; f++){
+if(f % 2 == 1)continue;
+console.log(f);
+}
+
+let g = 8;
+while( g <= 56 ){
+    if(g % 2 != 0){
+        g++;
+    } else{
+    console.log(g);
+    g++;
+}
+}
+//6
+for(let t = 2; t <=10; t++){
+    sum = 0;
+    for(let d = 1; d <=10; d++){
+        sum = t * d;
+        console.log(t + " * " + d + " = " + sum);
+    }
+}
+
+
+
+
+
